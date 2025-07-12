d5af2b16c9323e363e3365cc93b3a44641627698
Valentino Giudice <giudicevalentino@gmail.com>
2025-07-12 14:40:30 +0800

    fix: add missing prisma schema and runtime deps, resolve service injection error

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TodoModule } from './todo/todo.module';
 
+@Global()
 @Module({
   imports: [PrismaModule, TodoModule],
   controllers: [AppController],
