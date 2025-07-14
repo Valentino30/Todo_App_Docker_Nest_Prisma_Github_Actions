f809688a05f75f965e34588a400dc28ff5f19406
Valentino Giudice <giudicevalentino@gmail.com>
2025-07-14 15:48:58 +0800

    feat: update create and edit todo dto

--- backend/src/todo/dto/create-todo.dto.ts
+++ backend/src/todo/dto/create-todo.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateTodoDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty({ message: 'Title cannot be empty' })
   title!: string;
